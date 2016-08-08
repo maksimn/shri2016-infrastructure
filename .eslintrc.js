187aeaa6ce1598c7b13cd1c0e3a2c342c8bd6276
maksimn <maksim1989sovunion@yandex.ru>
2016-08-08 10:46:45 +0300

    Add eslint to repo

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
