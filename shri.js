b1ade9374862a7e294ac7c9070c135e5daf84291
maksimn <maksim1989sovunion@yandex.ru>
2016-08-09 22:06:24 +0300

    Add tests

---
+++ shri.js
@@ -0,0 +1,25 @@
+﻿var SHRI = (function () {
+    function shri() {
+
+    }
+
+    shri.prototype.createTeam = function (teamName) {
+        return new Team(teamName);
+    };
+
+    shri.prototype.createStudent = function (studName) {
+        return new Student(studName);
+    };
+
+    function Team(name) {
+        this.name = name;
+    }
+
+    function Student(name) {
+        this.name = name;
+    }
+
+    return shri;
+}());
+
+module.exports = SHRI;
---
+++ test.js
@@ -0,0 +1,30 @@
+﻿var assert = require('assert');
+const SHRI = require('./shri.js');
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+    });
+});
+
+describe('SHRI', function () {
+    describe('#createTeam', function () {
+        it('value of name property of Team object must be the same as argument of SHRI.createTeam() method', function () {
+            var shri = new SHRI();
+            var team = shri.createTeam('A');
+            assert.equal('A', team.name);
+        });
+    });
+});
+
+describe('SHRI', function () {
+    describe('#createStudent', function () {
+        it('value of name property of Student object must be the same as argument of SHRI.createStudent() method', function () {
+            var shri = new SHRI();
+            var stud = shri.createStudent('a');
+            assert.equal('a', stud.name);
+        });
+    });
+});
